35be6c76d5887d6171705cec2e0f64d1d21ed4a7
shinjansengar <shinjansengar@gmail.com>
2021-08-14 20:04:45 +0530

    Initial commit

---
+++ WallpaperChange/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WallpaperChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hey");
+            string photo = args[1];
+            Console.WriteLine(args[1]);
+            DisplayPicture(photo);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SystemParametersInfo(uint uiAction, uint uiParam, String pvParam, uint fWinIni);
+
+        private const uint SPI_SETDESKWALLPAPER = 0x14;
+        private const uint SPIF_UPDATEINIFILE = 0x1;
+        private const uint SPIF_SENDWININICHANGE = 0x2;
+
+        private static void DisplayPicture(string file_name)
+        {
+            uint flags = 0;
+            if (!SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                    0, file_name, flags))
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
